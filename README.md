3b80ee103d77df6257f7972991eeb6ffdc402c69
zwd-admin <810095178@qq.com>
2018-12-14 14:44:50 +0800

    zwd

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 ![SpringBoot2.0集成WebSocket，实现后台向前端推送信息](https://blog.csdn.net/moshowgame/article/details/80275084)
 
 
+![使用Aop防止表单重复提交](./springboot-form-aop-demo)
---
+++ springboot-form-aop-demo/src/main/java/com/zwd/springboot/form/aop/Application.java
@@ -0,0 +1,12 @@
+package com.zwd.springboot.form.aop;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zwd
+ * @date 2018/12/14 10:41
+ * @Email stephen.zwd@gmail.com
+ */
+@SpringBootApplication
+public class Application {
+}
